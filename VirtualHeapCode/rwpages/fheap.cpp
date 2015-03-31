0122cb5aee8f795bc140958a21ddda12772fb460
ferllini_13 <ferllini_13@hotmail.com>
2015-03-31 16:18:52 -0600

    areglo fheap y vheap con vRef's, y modificacion del paper

--- VirtualHeapCode/rwpages/fheap.cpp
+++ VirtualHeapCode/rwpages/fheap.cpp
@@ -3,68 +3,70 @@
 #include <iostream>
 #include <stdio.h>
 #include "src/vobject.h"
+#include "src/vref.h"
+
 
 fHeap::fHeap(int overWeight){
     _isPage=false;
     _memoryPage=overWeight;
 }
 
-void fHeap::addToPage(registry nregistry,int weight){
+void fHeap::addToPage(vRef newvRef){
     if (!_isPage){
       this->deletePage();
       _isPage=true;
     }
 
-    if (_totalDataMemory+weight<=_memoryPage){
+    if (_totalDataMemory+newvRef.getWeight()<=_memoryPage){
         std::ofstream fileOut("/home/fran/Escritorio/proof.bin",
                               std::ios::out | std::ios::binary | std::ios::app);
 
-        fileOut.write(reinterpret_cast<char *>(&nregistry),sizeof(weight));
+        fileOut.write(reinterpret_cast<char *>(&newvRef),sizeof(newvRef.getWeight()));
 
         fileOut.close();
         _totalData++;
-        _totalDataMemory+=weight;
+        _totalDataMemory+=newvRef.getWeight();
+        _weightList.add(newvRef.getWeight());
     }
 }
 
+vRef fHeap::searchInPage(int id){
 
-registry fHeap::searchInPage(int id, int weight){
-
-    vObject regTmp(-1,-1);
-    VObject returnableRegistry(-1,-1);
+    vRef refTmp;//temporal
+    vRef returnableRef;//para retornar
     std::ofstream fileTmp("/home/fran/Escritorio/tmp.bin",std::ios::out | std::ios::binary |std::ios::app);
 
     std::ifstream fileIn("/home/fran/Escritorio/proof.bin",std::ios::in | std::ios::binary);
-    int x=0;
-    for(int i=0;i<_totalData;i++){
-        char chain[weight];
 
-        fileIn.read(reinterpret_cast<char *>(&chain),sizeof(vObject));
-        for(int j=0;j<weight;j++){
-            *((char*)((void*)(&regTmp))+ j)=chain[j];
+    int decreaseMemory=0;
+    int deletableItemWeight=-1;
+    for(int i=0;i<_totalData;i++){
+        char chain[_weightList.get(i)];//cambair weight por el de lista..
+
+        fileIn.read(reinterpret_cast<char *>(&chain),_weightList.get(i));//cambiar size!!
+        for(int j=0;j<_weightList.get(i);j++){
+            *((char*)((void*)(&refTmp))+ j)=chain[j];
         }
 
-        if(regTmp.id==id){
-            x=-1;
-            std::cout<<"entrada de if"<<std::endl;
-            std::cout<<_totalData<<"  data  antes"<<std::endl;
-            _totalDataMemory-=weight;
-            returnableRegistry=regTmp;
-
+        if(refTmp._Id==id){            //modificar condicion de comparacion!!!
+            decreaseMemory=-1;//decremento de todal data
+            _totalDataMemory-=_weightList.get(i);
+            returnableRef=refTmp;
+            deletableItemWeight=i;
         }
         else{
-            fileTmp.write(reinterpret_cast<char *>(&regTmp),sizeof(vObject));
-            std::cout<<regTmp.id<<"   id agregado al doc nuevo"<<std::endl;
+            fileTmp.write(reinterpret_cast<char *>(&refTmp),sizeof(_weightList.get(i)));//cambiar size segun lista!
+            decreaseMemory=0;
         }
     }
 
     fileIn.close();
     fileTmp.close();
+    if(deletableItemWeight!=-1)_weightList.remove(deletableItemWeight);
     remove("/home/fran/Escritorio/proof.bin");
     rename("/home/fran/Escritorio/tmp.bin","/home/fran/Escritorio/proof.bin");
-    _totalData+=x;
-    std::cout<<_totalData<<"   data final"<<std::endl;
-    return returnableRegistry;
+    _totalData+=decreaseMemory;
+    return returnableRef;
 
 }
 
--- VirtualHeapCode/rwpages/fheap.h
+++ VirtualHeapCode/rwpages/fheap.h
@@ -2,19 +2,22 @@
 #define FHEAP
 #include <string>
 #include "src/vobject.h"
+#include "src/vref.h"
+#include "list/List.h"
+#include "list/Node.h"
 
 
 class fHeap{
 private:
-
     bool _isPage=false;
     int _memoryPage;
     int _totalData=0;
     int _totalDataMemory=0;
+    List<int> _weightList;
 public:
     fHeap(int overWeight);
-    void addToPage(vObject newvObject,int Weight);
-    vObject searchInPage(int searchId,int weight);
+    void addToPage(vRef newvRef);
+    vRef searchInPage(int searchId);
     void deletePage();
     ~fHeap();
 
